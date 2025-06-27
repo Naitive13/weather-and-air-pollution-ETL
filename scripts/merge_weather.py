447797134bbf4e2633aeb92ce142cfb83c4509d3
Naitive13 <hei.titan.2@gmail.com>
2025-06-27 11:20:50 +0300

    feat: create scripts for merging data into separated spreadsheet

---
+++ scripts/merge_weather.py
@@ -0,0 +1,37 @@
+import gspread
+import logging
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def merge_weather_data(cities):
+    try:
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive",
+        ]
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            "../client_secret.json", scope
+        )
+        client = gspread.authorize(credentials)
+        spreadsheet = client.open("Weather-ETL").worksheet("Weather-ETL")
+
+        for city in cities:
+            try:
+                df = pd.read_csv(f"../data/weather_{city}", index_col=False)
+                for index, row in df.iterrows():
+                    spreadsheet.append_row(row.to_list())
+            except FileNotFoundError:
+                logging.error(f"file 'weather_{city}' not found, skipping...")
+                continue
+            except Exception as e:
+                logging.error(f"Error processing {city}: {str(e)}")
+                continue
+
+        return True
+    except Exception as e:
+        print(f"Error while merging weather data: {str(e)}")
+        return False
