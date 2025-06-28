4c906f558b7a4a8de9ac86403d84165c7a09de41
Naitive13 <hei.titan.2@gmail.com>
2025-06-28 09:45:26 +0300

    feat: create __init__.py

---
+++ scripts/__init__.py
@@ -0,0 +1,19 @@
+# from extract_air_pollution import get_current_air_pollution_data
+# from extract_weather import get_current_weather_data
+# from merge_air_pollution import merge_air_pollution_data
+# from merge_weather import merge_weather_data
+from . import (
+    extract_air_pollution,
+    extract_weather,
+    merge_air_pollution,
+    merge_weather,
+    get_coordinates,
+)
+
+__all__ = [
+    "extract_air_pollution",
+    "extract_weather",
+    "merge_weather",
+    "merge_air_pollution",
+    "get_coordinates",
+]
