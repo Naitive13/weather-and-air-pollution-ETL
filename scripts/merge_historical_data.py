82adb015d3939d363ad72c662766006cd4e16b26
Naitive13 <hei.titan.2@gmail.com>
2025-07-04 16:19:51 +0300

    feat: add function to append data to a google spreadsheet

--- scripts/merge_historical_data.py
+++ scripts/merge_historical_data.py
@@ -25,3 +25,25 @@ def merge_historical_air_pollution_data(data, city):
     except Exception as e:
         logging.error(f"Error while merging air pollution data: {str(e)}")
         return False
+
+
+def merge_historical_weather_data(data, city):
+    try:
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive",
+        ]
+
+        path = Path(__file__).parent / "../client_secret.json"
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
+        client = gspread.authorize(credentials)
+        spreadsheet = client.open("Weather-ETL").worksheet(
+            f"History-{city.replace(' ', '_')}"
+        )
+        spreadsheet.append_rows(data)
+        return True
+    except Exception as e:
+        logging.error(f"Error while merging weather data: {str(e)}")
+        return False
